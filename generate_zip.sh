43ebd1df16f911b076505d0780519d4336143edf
Arno Cellarier <git@arno.cl>
2024-11-05 11:58:14 +0100

    fix: fares

--- generate_zip.sh
+++ generate_zip.sh
@@ -1,4 +1,7 @@
+# generate default zip file
 python3 generate.py
+
+# add shapes and generate new zip file
 wget -O osm/cyprus-latest.osm.bz2 https://download.geofabrik.de/europe/cyprus-latest.osm.bz2
 docker pull ghcr.io/ad-freiburg/pfaedle:latest
 docker run -i --rm \
@@ -7,6 +10,17 @@ docker run -i --rm \
     --volume "$(pwd)/output:/gtfs-out" \
     ghcr.io/ad-freiburg/pfaedle:latest \
     -x /osm/cyprus-latest.osm.bz2 -i /gtfs/myfeed.zip
-rm gtfs.zip
+
+# unzip fare_attributes.txt and fare_rules.txt from gtfs.zip
+unzip gtfs.zip -d gtfs
+
+# replace fare_attributes.txt and fare_rules.txt in output folder
+cp gtfs/fare_attributes.txt output/fare_attributes.txt
+cp gtfs/fare_rules.txt output/fare_rules.txt
+
+# remove tmp folders
 rm -f output.zip
+rm -f gtfs.zip
+rm -rf gtfs
+
 zip -j output.zip output/*.txt
